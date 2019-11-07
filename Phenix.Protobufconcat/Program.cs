76017f5142c61ad9a5c0f81a1be3d213092e6fbc
JD <jdphenix@gmail.com>
2019-11-06 22:31:34 -0600

    Times serialization/deserialization operations
    I've updated the ReadStreams and WriteStreams
    methods to time how long the respective operations
    take.
    
    For reading, I'm checking elapsed time after the
    execution of the loop but before the stream is
    closed, because I figure the work I care about
    there is already done.
    
    For writing, I'm checking elapsed time after the
    stream is closed, because I'd consider time to
    flush to disk important for serialization. This
    may be less important for either memory backed
    stream (effectively eliminating the flush time)
    or a network backed stream (making 99.9% of
    the operation time *be* the network), both
    things worth checking.
    
    For both operations, I clearly need to learn
    how to properly use the new JSON API. Questions
    for myself include,
    
    1) Does myself as a user of the new System.Text
    JSON API need to familiarize myself with
    Span<T> and Memory<T> to effectively use it, or
    is just pointing the results of a StreamReader
    sufficient to gain it's performance benefits?
    2) What about more complex data, like a series
    of more complex object graphs (think `contacts`,
    `job history`, `relationships` for people, in
    this example).

--- Phenix.Protobufconcat/Program.cs
+++ Phenix.Protobufconcat/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using Google.Protobuf;
-using Phenix.ProtobufConcat.Messages;
 using RandomDataGenerator.FieldOptions;
 using RandomDataGenerator.Randomizers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Diagnostics;
+using Phenix.ProtobufConcat.Messages;
 
 /// <summary>
 /// I created to project to demonstrate serializing and deserializing
@@ -18,38 +19,50 @@ namespace Phenix.Protobufconcat
 {
     class Program
     {
+        const int count = 1000;
+
         static void Main(string[] args)
         {
+
             var randomFirstNameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsFirstName());
             var randomLastNameGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsLastName());
             var randomDateGenerator = RandomizerFactory.GetRandomizer(new FieldOptionsDateTime());
 
-            const int count = 1000000;
-            var list = new List<Person>(count);
-            for (var i = 0; i < count; i++)
+            for (var o = 10; o <= 10000; o *= 10)
             {
-                var offset = (DateTimeOffset)randomDateGenerator.Generate().Value;
-
-                list.Add(new Person
+                var list = new List<Person>(count);
+                var batchCount = count * o;
+                Console.Write($"{batchCount} ");
+                for (var i = 0; i < batchCount; i++)
                 {
-                    Birthdate = offset.ToUnixTimeSeconds(),
-                    LastName = randomLastNameGenerator.Generate(true),
-                    FirstName = randomFirstNameGenerator.Generate(true)
-                });
+                    var offset = (DateTimeOffset)randomDateGenerator.Generate().Value;
+
+                    list.Add(new Person
+                    {
+                        Birthdate = offset.ToUnixTimeSeconds(),
+                        LastName = randomLastNameGenerator.Generate(true),
+                        FirstName = randomFirstNameGenerator.Generate(true)
+                    });
+                }
+
+                var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var id = Guid.NewGuid().ToString();
+
+                WriteStreams(list, desktopPath, id);
+                ReadStreams(list, desktopPath, id);
+                Console.WriteLine();
             }
 
-            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            var id = Guid.NewGuid().ToString();
-
-            WriteStreams(list, desktopPath, id);
-            ReadStreams(list, desktopPath, id);
         }
+
 
         private static void ReadStreams(List<Person> list, string path, string id)
         {
             // read back from the streams and verify
-            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.dat")))
+            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.{count}.dat")))
             {
+                var timer = Stopwatch.StartNew();
+                Console.Write(",read-proto:");
                 foreach (var p in list)
                 {
                     var personSerialized = Person.Parser.ParseDelimitedFrom(fs);
@@ -58,11 +71,14 @@ namespace Phenix.Protobufconcat
                         Console.Error.WriteLine($"Unexpected message not equal, {p}, {personSerialized}");
                     }
                 }
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
 
-            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.compressed.dat")))
+            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.{count}.compressed.dat")))
             using (var cs = new DeflateStream(fs, CompressionMode.Decompress))
             {
+                var timer = Stopwatch.StartNew();
+                Console.Write(",read-compressed-proto:");
                 foreach (var p in list)
                 {
                     var personSerialized = Person.Parser.ParseDelimitedFrom(cs);
@@ -71,44 +87,79 @@ namespace Phenix.Protobufconcat
                         Console.Error.WriteLine($"Unexpected message not equal, {p}, {personSerialized}");
                     }
                 }
+                Console.Write($"{timer.ElapsedMilliseconds}");
+            }
+
+            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.{count}.json")))
+            using (var streamReader = new StreamReader(fs))
+            {
+                var timer = Stopwatch.StartNew();
+                Console.Write(",read-json:");
+                var listFromFile = JsonSerializer.Deserialize<List<Person>>(streamReader.ReadToEnd());
+                for (var i = 0; i < list.Count; i++) if (!list[i].Equals(listFromFile[i])) throw new Exception();
+                Console.Write($"{timer.ElapsedMilliseconds}");
+            }
+
+            using (var fs = File.OpenRead(Path.Combine(path, "wkp", $"{id}.{count}.compressed.json")))
+            using (var cs = new DeflateStream(fs, CompressionMode.Decompress))
+            using (var streamReader = new StreamReader(cs))
+            {
+                var timer = Stopwatch.StartNew();
+                Console.Write(",read-compressed-json:");
+                var listFromFile = JsonSerializer.Deserialize<List<Person>>(streamReader.ReadToEnd());
+                for (var i = 0; i < list.Count; i++) if (!list[i].Equals(listFromFile[i])) throw new Exception();
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
         }
 
         private static void WriteStreams(List<Person> list, string path, string id)
         {
+            var timer = Stopwatch.StartNew();
             // Just a normal stream.
-            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.dat")))
+            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.{count}.dat")))
             {
+                Console.Write(",write-proto:");
                 foreach (var p in list)
                 {
                     p.WriteDelimitedTo(fs);
                 }
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
 
             // (deflate) compressed stream
-            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.compressed.dat")))
+            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.{count}.compressed.dat")))
             using (var cs = new DeflateStream(fs, CompressionLevel.Optimal))
             {
+                timer = Stopwatch.StartNew();
+                Console.Write(",write-compressed-proto:");
                 foreach (var p in list)
                 {
                     p.WriteDelimitedTo(cs);
                 }
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
 
             // Just a normal stream.
-            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.json")))
+            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.{count}.json")))
             using (var writer = new StreamWriter(fs))
             {
+                timer = Stopwatch.StartNew();
+                Console.Write(",write-json:");
                 writer.Write(JsonSerializer.Serialize(list));
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
 
             // (deflate) compressed stream
-            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.compressed.json")))
+            using (var fs = File.Create(Path.Combine(path, "wkp", $"{id}.{count}.compressed.json")))
             using (var cs = new DeflateStream(fs, CompressionLevel.Optimal))
             using (var writer = new StreamWriter(cs))
             {
+                timer = Stopwatch.StartNew();
+                Console.Write(",write-compressed-json:");
                 writer.Write(JsonSerializer.Serialize(list));
+                Console.Write($"{timer.ElapsedMilliseconds}");
             }
         }
+
     }
 }
